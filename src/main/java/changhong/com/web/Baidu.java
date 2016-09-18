e45a9e0408af9fe7414fbfe1a8cf1a803acd15bc
长宏 <3200601@qq.com>
2016-09-18 14:59:06 +0800

    正确，但是boot打包不行，ecliipse打包可以运行

--- src/main/java/changhong/com/web/Baidu.java
+++ src/main/java/changhong/com/web/Baidu.java
@@ -6,7 +6,6 @@ package changhong.com.web;
 import java.io.UnsupportedEncodingException;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,24 +19,6 @@ import com.baidu.ueditor.ActionEnter;
  */
 @RestController
 public class Baidu {
-
-	@RequestMapping(value = "222baidu")
-	public String getmessaget(HttpServletRequest request, HttpServletResponse response) {
-		try {
-			request.setCharacterEncoding("utf-8");
-		} catch (UnsupportedEncodingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		response.setHeader("Content-Type", "text/html");
-
-		String rootPath = request.getServletContext().getRealPath("/");
-
-		return new ActionEnter(request, rootPath).exec();
-		// return "controller";
-
-	}
-
 	@RequestMapping(value = "jsp/{path}.jsp")
 	public String get(@PathVariable String path, HttpServletRequest request) {
 
@@ -56,10 +37,5 @@ public class Baidu {
 		return dd;
 
 	}
-	// @RequestMapping(value = "jsp/{path}.jsp")
-	// public String doit(@PathVariable String path) {
-	// System.out.println(path);
-	// return path;
-	// }
 
 }
--- src/main/java/changhong/com/web/WelcomeController.java
+++ src/main/java/changhong/com/web/WelcomeController.java
@@ -1,20 +1,20 @@
 package changhong.com.web;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class WelcomeController {
 
-	@RequestMapping("/22gggggff")
+	@RequestMapping("/22ggg.ggff")
 	public String welcome() {
 		System.out.println("hello");
 		return "controller";
 	}
 
 	@ResponseBody
-	@RequestMapping("/22ss")
+	@RequestMapping("/22s.s")
 	public WelcomeController welcome1() {
 		return this;
 	}
