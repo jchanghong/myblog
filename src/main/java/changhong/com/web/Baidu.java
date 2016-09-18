0cfdfc862489ff51e0da25efc4bebc5810049eb3
长宏 <3200601@qq.com>
2016-09-18 10:48:27 +0800

    Signed-off-by: 长宏 <3200601@qq.com>

--- src/main/java/changhong/com/web/Baidu.java
+++ src/main/java/changhong/com/web/Baidu.java
@@ -8,9 +8,9 @@ import java.io.UnsupportedEncodingException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.baidu.ueditor.ActionEnter;
 
@@ -18,7 +18,7 @@ import com.baidu.ueditor.ActionEnter;
  * @author jiang
  *
  */
-@Controller
+@RestController
 public class Baidu {
 
 	@RequestMapping(value = "222baidu")
@@ -39,9 +39,27 @@ public class Baidu {
 	}
 
 	@RequestMapping(value = "jsp/{path}.jsp")
-	public String doit(@PathVariable String path) {
-		System.out.println(path);
-		return path;
+	public String get(@PathVariable String path, HttpServletRequest request) {
+
+		try {
+			request.setCharacterEncoding("utf-8");
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		// response.setHeader("Content-Type" , "text/html");
+
+		String rootPath = request.getServletContext().getRealPath("/");
+
+		String dd = new ActionEnter(request, rootPath).exec();
+		System.out.println(dd);
+		return dd;
+
 	}
+	// @RequestMapping(value = "jsp/{path}.jsp")
+	// public String doit(@PathVariable String path) {
+	// System.out.println(path);
+	// return path;
+	// }
 
 }
